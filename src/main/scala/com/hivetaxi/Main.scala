2105fcef531d04627e429218ead6e6d883320b33
RomanGoltsov <romer533@mail.ru>
2021-09-20 17:16:46 +0600

    Impl Eq for Cat

--- src/main/scala/com/hivetaxi/Main.scala
+++ src/main/scala/com/hivetaxi/Main.scala
@@ -1,7 +1,11 @@
 package com.hivetaxi
 
+import cats.implicits.toShow
+import cats.instances.int._
 import cats.instances.string._
+import cats.syntax.eq._
 import cats.syntax.semigroup._
+import cats.{Eq, Show}
 import com.hivetaxi.part.first.PrintableInstances._
 import com.hivetaxi.part.first.PrintableSyntax.PrintableOps
 import com.hivetaxi.part.first.{Cat, Printable}
@@ -20,5 +24,20 @@ object Main extends App {
 
   // Better Syntax
   cat.print
+
+  // Cat Show
+  implicit val showFromCat: Show[Cat] = Show.show[Cat] { value =>
+    s"${value.name} is a ${value.age} year-old ${value.color} cat."
+  }
+  println(cat.show)
+
+  // Equality, liberty, and felinity
+  implicit val eqFromCat: Eq[Cat] = Eq.instance[Cat] { (value1, value2) =>
+    value1.name === value2.name && value1.age === value2.age && value1.color === value2.color
+  }
+  val cat1 = cat
+  val cat2 = Cat("Leva", 3, "Black")
+  println(cat1 === cat2)
+  println(cat1 =!= cat2)
 
 }
