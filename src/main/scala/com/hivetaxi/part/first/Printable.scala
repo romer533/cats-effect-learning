c1bca14edd83db056090ff7a59fbcb4bc7d3b37e
RomanGoltsov <romer533@mail.ru>
2021-09-20 16:05:47 +0600

    Impl printable type class

---
+++ src/main/scala/com/hivetaxi/part/first/Printable.scala
@@ -0,0 +1,16 @@
+package com.hivetaxi.part.first
+
+trait Printable[A] {
+  def format(value: A): String
+}
+
+object PrintableInstances {
+  implicit val printableString: Printable[String] = (value: String) => value
+  implicit val printableInt: Printable[Int] = (value: Int) => value.toString
+}
+
+object Printable {
+  def format[A](value: A)(implicit p: Printable[A]): String = p.format(value)
+  def print[A](value: A)(implicit p: Printable[A]): Unit =
+    println(format(value))
+}
