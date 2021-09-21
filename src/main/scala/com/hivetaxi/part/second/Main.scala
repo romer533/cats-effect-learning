d8da3542522e47079f938014640e6408460667c1
RomanGoltsov <romer533@mail.ru>
2021-09-21 09:16:25 +0600

    Adding all the things

--- src/main/scala/com/hivetaxi/part/second/Main.scala
+++ src/main/scala/com/hivetaxi/part/second/Main.scala
@@ -1,7 +1,9 @@
 package com.hivetaxi.part.second
 
 import cats.Monoid
+import cats.implicits.catsKernelStdGroupForDouble
 import cats.kernel.Semigroup
+import cats.syntax.semigroup._
 
 object Main extends App {
 
@@ -50,5 +52,18 @@ object Main extends App {
       override def combine(x: Set[A], y: Set[A]): Set[A] =
         (x diff y) union (y diff x)
     }
+
+  // Adding all the things
+  implicit val monoidOrder: Monoid[Order] =
+    new Monoid[Order] {
+      override def empty: Order = Order(0, 0)
+      override def combine(x: Order, y: Order): Order =
+        Order((x.totalCost |+| y.totalCost), (x.quantity |+| y.quantity))
+    }
+
+  def add[A](items: List[A])(implicit m: Monoid[A]): A =
+    items.foldLeft(m.empty)(_ |+| _)
+
+  println(add(List(Order(1.0, 2.0), Order(3.0, 1.0))))
 
 }
