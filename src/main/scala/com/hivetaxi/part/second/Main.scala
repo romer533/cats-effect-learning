c2f7eacd31d45e24e3e604ff96c8cd2e4d3e9244
RomanGoltsov <romer533@mail.ru>
2021-09-21 09:23:21 +0600

    Update Main.scala

--- src/main/scala/com/hivetaxi/part/second/Main.scala
+++ src/main/scala/com/hivetaxi/part/second/Main.scala
@@ -58,7 +58,7 @@ object Main extends App {
     new Monoid[Order] {
       override def empty: Order = Order(0, 0)
       override def combine(x: Order, y: Order): Order =
-        Order((x.totalCost |+| y.totalCost), (x.quantity |+| y.quantity))
+        Order(x.totalCost |+| y.totalCost, x.quantity |+| y.quantity)
     }
 
   def add[A](items: List[A])(implicit m: Monoid[A]): A =
