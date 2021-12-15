a883ed2ae4d93c79283f97b00d55a475364fa4b6
RomanGoltsov <romer533@mail.ru>
2021-12-15 13:22:24 +0600

    Better Syntax

---
+++ src/main/scala/com/hivetaxi/part/printable_library/PrintableSyntax.scala
@@ -0,0 +1,8 @@
+package com.hivetaxi.part.printable_library
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit p: Printable[A]): String = p.format(value)
+    def print(implicit p: Printable[A]): Unit = println(format(p))
+  }
+}
