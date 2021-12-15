14ba34e7ead1854dd48c01e6e27ec934705851e2
RomanGoltsov <romer533@mail.ru>
2021-12-15 13:22:39 +0600

    Better Syntax (#10)

--- src/main/scala/com/hivetaxi/part/printable_library/Main.scala
+++ src/main/scala/com/hivetaxi/part/printable_library/Main.scala
@@ -1,8 +1,9 @@
 package com.hivetaxi.part.printable_library
 
 import com.hivetaxi.part.printable_library.PrintableInstances.toPrintableCat
+import com.hivetaxi.part.printable_library.PrintableSyntax.PrintableOps
 
 object Main extends App {
   val cat = Cat("Bars", 7, "Black")
-  Printable.print(cat)
+  cat.print
 }
